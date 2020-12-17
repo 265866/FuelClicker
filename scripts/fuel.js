d3411247bdf00e799e48274a69573eb3f103ea57
265866 <54053306+265866@users.noreply.github.com>
2020-12-17 09:36:48 -0800

    giveFuel()

--- scripts/fuel.js
+++ scripts/fuel.js
@@ -1,23 +1,27 @@
 var fuel = 0
 var fuelPerSecond = 0
 
+function begin()
+{
+    updateCounters();
+    setTimeout(begin, 10, window);
+}
+
 function fuelClick() {
     fuel++
     console.log(fuel)
 }
 
-function updateCounters()
-{    
+function updateCounters() {    
     var fuelcount = document.getElementById('fuelcount');
     fuelcount.innerHTML = "fuel: " + fuel;
     var fuelsecond = document.getElementById('fuelpersecond')
     fuelsecond.innerHTML = "Fuel per Second: " + fuelPerSecond
 }
 
-function begin()
-{
-    updateCounters();
-    setTimeout(begin, 10, window);
+function giveFuel() {
+    fuel = fuel + fuelPerSecond
+    setTimeout(giveFuel, 1000)
 }
 
 document.addEventListener('DOMContentLoaded', function () {    
