2efe27de22316eca94f5ce5fb942d20971728c57
265866 <54053306+265866@users.noreply.github.com>
2021-01-05 09:32:07 -0800

    redid everything

--- scripts/fuel.js
+++ scripts/fuel.js
@@ -1,3 +1,12 @@
+//TO DO TOMORROW
+
+//TUTORIAL (using alerts; have to figure out options and stuff)
+//DOWNLOAD 10 MUSIC TRACKS
+//ADD PLAY/PAUSE BUTTON (actual icon lol - also positioning)
+//RANDOMIZE TRACKS & PLAY (see startMusic())
+//ADD A NOWPLAYING TEXTBOX
+
+
 var fuel = 0
 var fuelPerSecond = 0
 var pickaxes = 0
@@ -20,17 +29,106 @@ var javascriptConsole = 0
 var javascriptConsoleCost = 500000
 var sellSound = new Audio('./sfx/SellSound.mp3')
 var click = new Audio('./sfx/click.mp3')
+var cantafford = new Audio('./sfx/cantafford.mp3')
+var intro = document.querySelector('.intro')
+var logo = document.querySelector('.logo-header')
+var logoSpan = document.querySelectorAll('.logo')
+var song1 = new Audio('./music/Astroblk - Nothing Really matters that much to me anymore.mp3')
+var song2 = new Audio('./music/Astroblk - Vancouver.mp3')
+var song3 = new Audio('./music/Aviscerall - Sanctuary.mp3')
+var song4 = new Audio('./music/Aviscerall - Coffee Shop.mp3')
+var song5 = new Audio('./music/Aviscerall - Sanctuary.mp3')
+var song6 = new Audio('./music/Whatever You Want (feat. Astroblk).mp3')
+var sound = false
 
+//function svglength() {
+//    const logo = document.querySelectorAll('#logo path');
+//    for (let i = 0; i < logo.length; i++){
+//        console.log('Letter ' + i + ' is ' + logo[i].getTotalLength());
+//    }
+//}
 
 function begin() {
     updateStuff();
     giveFuel();
     checkForAfford();
+    cheatPrevention();
+    tutorial();
+    //svglength();
 }
 
 function fuelClick() {
     fuel++
     click.play()
+}
+
+function tutorial() {
+    setTimeout(() => {
+        alert("Welcome to Space Clicker!");
+        if (confirm("Would you like a tutorial?")) {
+            alert("Okay, to get started, click on the fuel icon a few times")
+            alert("When you get to 100 fuel, click on the Pickaxe upgrade.")
+            alert("The goal of this game is to get as much fuel as possible.")
+            alert("Now that you've learned the basics, it's time for you to get started!")
+          } else {
+            txt = "You pressed Cancel!";
+          }
+    }, 10000)
+}
+
+function cheatPrevention() {
+    console.log("Ahh, well you came here to cheat.")
+    console.log("We expected that, and there's really nothing we can do to stop you")
+    console.log("So, we made it easier for you...")
+    console.log("Actually, we have a full function dedicated to you!")
+    console.log("Use the function 'ruinTheFun()' to unlock everything")
+}
+
+function ruinTheFun() {
+    console.log(".")
+    console.log(".")
+    console.log(".")
+    console.log("You actually expected that to work??")
+    console.log("Instead- we destroyed all of your work!")
+    console.log("Here are your current stats:")
+    console.log(".")
+    console.log("Fuel: " + fuel)
+    console.log("Fuel Per Second: " + fuelPerSecond)
+    console.log("Pickaxes: " + pickaxes)
+    console.log("Miners: " + miners)
+    console.log("Army Of Miners: " + armyOfMiners)
+    console.log("Fuel Mines: " + fuelMines)
+    console.log("Fuel Factories: " + fuelFactories)
+    console.log("Fuel Banks: " + fuelBank)
+    console.log("Fuel Temple: " + fuelTemple)
+    console.log("Fuel Time Machines: " + fuelTimeMachine)
+    console.log("Javascript Consoles: " + javascriptConsole)
+    console.log(".")
+    console.log("And here are your new and improved stats!")
+    console.log(".")
+    fuel = 0
+    fuelPerSecond = 0
+    pickaxes = 0
+    miners = 0
+    armyOfMiners = 0
+    fuelMines = 0
+    fuelFactories = 0
+    fuelBank = 0
+    fuelTemple = 0
+    fuelTimeMachine = 0
+    javascriptConsole = 0
+    console.log("Fuel: " + fuel)
+    console.log("Fuel Per Second: " + fuelPerSecond)
+    console.log("Pickaxes: " + pickaxes)
+    console.log("Miners: " + miners)
+    console.log("Army Of Miners: " + armyOfMiners)
+    console.log("Fuel Mines: " + fuelMines)
+    console.log("Fuel Factories: " + fuelFactories)
+    console.log("Fuel Banks: " + fuelBank)
+    console.log("Fuel Temple: " + fuelTemple)
+    console.log("Fuel Time Machines: " + fuelTimeMachine)
+    console.log("Javascript Consoles: " + javascriptConsole)
+    return ("have fun :)")
 }
 
 function updateStuff() {
@@ -123,7 +221,9 @@ function pickaxe() {
         sellSound.play()
         pickaxeCost = Math.round(pickaxeCost)
         pickaxebutton.innerHTML = 'Pickaxe: +1 FPS<br/>Cost: ' + pickaxeCost + '<br/>Obtained: ' + pickaxes
-    } else {}
+    } else {
+        cantafford.play()
+    }
 }
 
 function miner() {
@@ -135,7 +235,9 @@ function miner() {
         sellSound.play()
         minerCost = Math.round(minerCost)
         minerbutton.innerHTML = 'Miner: +5 FPS<br/>Cost: ' + minerCost + '<br/>Obtained: ' + miners
-    } else {}
+    } else {
+        cantafford.play()
+    }
 }
 
 function armyofminers() {
@@ -147,7 +249,9 @@ function armyofminers() {
         sellSound.play()
         armyOfMinerCost = Math.round(armyOfMinerCost)
         armyofminersbutton.innerHTML = 'Armies Of Miners: +20 FPS<br/>Cost: ' + armyOfMinerCost + '<br/>Obtained: ' + armyOfMiners
-    } else {}
+    } else {
+        cantafford.play()
+    }
 }
 
 function fuelmine() {
@@ -159,7 +263,9 @@ function fuelmine() {
         sellSound.play()
         fuelMineCost = Math.round(fuelMineCost)
         fuelminebutton.innerHTML = 'Fuel Mines: +50 FPS<br/>Cost: ' + fuelMineCost + '<br/>Obtained: ' + fuelMines
-    } else {}
+    } else {
+        cantafford.play()
+    }
 }
 
 function fuelfactory() {
@@ -171,7 +277,9 @@ function fuelfactory() {
         sellSound.play()
         fuelFactoryCost = Math.round(fuelFactoryCost)
         fuelfactorybutton.innerHTML = 'Fuel Factories: +100 FPS<br/>Cost: ' + fuelFactoryCost + '<br/>Obtained: ' + fuelFactories
-    } else {}
+    } else {
+        cantafford.play()
+    }
 }
 
 function fuelbank() {
@@ -183,7 +291,9 @@ function fuelbank() {
         sellSound.play()
         fuelBankCost = Math.round(fuelBankCost)
         fuelbankbutton.innerHTML = 'Fuel Banks: +200 FPS<br/>Cost: ' + fuelBankCost + '<br/>Obtained: ' + fuelBank
-    } else {}
+    } else {
+        cantafford.play()
+    }
 }
 
 function fueltemple() {
@@ -195,7 +305,9 @@ function fueltemple() {
         sellSound.play()
         fuelTempleCost = Math.round(fuelTempleCost)
         fueltemplebutton.innerHTML = 'Fuel Temples: +500 FPS<br/>Cost: ' + fuelTempleCost + '<br/>Obtained: ' + fuelTemple
-    } else {}
+    } else {
+        cantafford.play()
+    }
 }
 
 function fueltimemachine() {
@@ -207,7 +319,9 @@ function fueltimemachine() {
         sellSound.play()
         fuelTimeMachineCost = Math.round(fuelTimeMachineCost)
         fueltimemachinebutton.innerHTML = 'Time Machines: +1000 FPS<br/>Cost: ' + fuelTimeMachineCost + '<br/>Obtained: ' + fuelTimeMachine
-    } else {}
+    } else {
+        cantafford.play()
+    }
 }
 
 function javascriptconsole() {
@@ -219,9 +333,75 @@ function javascriptconsole() {
         sellSound.play()
         fuelTempleCost = Math.round(fuelTempleCost)
         javascriptconsolebutton.innerHTML = 'Javascript Consoles: +5000 FPS<br/>Cost: ' + javascriptConsoleCost + '<br/>Obtained: ' + javascriptConsole
-    } else {}
+    } else {
+        cantafford.play()
+    }
+}
+
+function startMusic() {
+
+    if (sound = true) {
+        
+    }
+    var x = Math.random()
+    x = x * 10
+    x = Math.round(x)
+    console.log(x)
+    if (x == 0) {
+        song1.play()
+        nowplaying.innerHTML = 'Now Playing: Astroblk - Nothing Really matters that much to me anymore'
+    }
+    if (x == 1) {
+        song2.play()
+        nowplaying.innerHTML = 'Now Playing: Astroblk - Vancouver'
+    }
+    if (x == 2) {
+        song3.play()
+        nowplaying.innerHTML = 'Now Playing: Aviscerall - Sanctuary'
+    }
+    if (x == 3) {
+        song4.play()
+        nowplaying.innerHTML = 'Now Playing: Aviscerall - Coffee Shop'
+    }
+    if (x == 4) {
+        song5.play()
+        nowplaying.innerHTML = 'Now Playing: Aviscerall - Sanctuary'
+    }
+    if (x == 5) {
+        song6.play()
+        nowplaying.innerHTML = 'Whatever You Want (feat. Astroblk)'
+    }
+    if (x == 6) {
+        song1.play()
+        nowplaying.innerHTML = 'Now Playing: Astroblk - Nothing Really matters that much to me anymore'
+    }
+    if (x == 7) {
+        song2.play()
+        nowplaying.innerHTML = 'Now Playing: Astroblk - Vancouver'
+    }
+    if (x == 8) {
+        song3.play()
+        nowplaying.innerHTML = 'Now Playing: Aviscerall - Sanctuary'
+    }
+    if (x == 9) {
+        song5.play()
+        nowplaying.innerHTML = 'Now Playing: Aviscerall - Sanctuary'
+    }
 }
 
 document.addEventListener('DOMContentLoaded', function() {
     begin();
 });
+
+document.addEventListener('DOMContentLoaded', () => {
+    setTimeout(() => {
+        logoSpan.forEach((span, idx) => {
+            setTimeout(() => {
+                span.classList.add('active');
+            }, (idx + 1) * 400)
+        });
+        setTimeout(() => {
+            intro.style.top = '-100vh';
+        }, 8500)
+    })
+})
